24742a64d0ed40fac92ec479782730c426a415a1
∧° ┐ <we.are.pe.hu@gmail.com>
2023-11-13 18:25:39 +0900

    Delete show.js

--- www/show.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-async function showJSON(requestURL) {
-    const request = new Request(requestURL)
-    const response = await fetch(request)
-    const indexAll = await response.text()
-    const json = JSON.parse(indexAll)
-    indexShow(json)
-}
